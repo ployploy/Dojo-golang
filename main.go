abd7ae2550bf625450bc3a57dedafc2881a54da2
ploy <chonnikan.t@kkumail.com>
2018-02-05 23:14:47 +0700

    create type string by golang

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x string = "hello"
+	var y string = "world"
+	fmt.Println(x == y)
+
+	xp := "Max"
+	fmt.Println("My dog's name is", xp)
+}
